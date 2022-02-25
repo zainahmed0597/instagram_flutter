d343e9671bb9fef22e7fca98638ca7c257f2b249
zain <zainahmed0597@gmail.com>
2022-02-26 02:05:47 +0500

    nothing

---
+++ lib/models/user.dart
@@ -0,0 +1,41 @@
+class User {
+  final String email;
+  final String uid;
+  final String username;
+  final String bio;
+  final String photoUrl;
+  final List followers;
+  final List following;
+  const User({
+    required this.email,
+    required this.uid,
+    required this.photoUrl,
+    required this.username,
+    required this.bio,
+    required this.followers,
+    required this.following,
+  });
+  // static User fromSnap(DocumentSnapshot snap) {
+  //   var snapshot = snap.data() as Map<String, dynamic>;
+  //
+  //   return User(
+  //     username: snapshot["username"],
+  //     uid: snapshot["uid"],
+  //     email: snapshot["email"],
+  //     photoUrl: snapshot["photoUrl"],
+  //     bio: snapshot["bio"],
+  //     followers: snapshot["followers"],
+  //     following: snapshot["following"],
+  //   );
+  // }
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "uid": uid,
+        "email": email,
+        "photoUrl": photoUrl,
+        "bio": bio,
+        "followers": followers,
+        "following": following,
+      };
+}
--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_flutter/models/user.dart' as model;
 import 'package:instagram_flutter/resources/storage_methods.dart';
 
 class AuthMethods {
@@ -30,17 +31,19 @@ class AuthMethods {
 
         String photoUrl = await StorageMethods()
             .uploadImageToStorage('profilePics', file, false);
-
         // add user to our database
-        await _firestore.collection('user').doc(cred.user!.uid).set({
-          'username': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl,
-        });
+        model.User user = model.User(
+          username: username,
+          uid: cred.user!.uid,
+          email: email,
+          bio: bio,
+          followers: [],
+          following: [],
+          photoUrl: photoUrl,
+        );
+        await _firestore.collection('user').doc(cred.user!.uid).set(
+              user.toJson(),
+            );
         //
         // await _firestore.collection('user').add({
         //   'username': username,
